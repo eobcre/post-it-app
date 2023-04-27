56ec9123f613c025cc229137a9151818c6188ce2
eobcre <eobcre@gmail.com>
2023-04-27 15:14:43 -0400

    note styled

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,10 +2,11 @@ import { RiDeleteBin2Line } from 'react-icons/Ri';
 
 const Note = () => {
   return (
-    <div>
-      <footer>
+    <div className='flex flex-col justify-between bg-neon-yellow rounded-md w-72 h-72 p-5'>
+      <h1>Title</h1>
+      <footer className='flex justify-between items-center'>
         <small>Date</small>
-        <RiDeleteBin2Line className='text-3xl' />
+        <RiDeleteBin2Line className='text-2xl' />
       </footer>
     </div>
   );
