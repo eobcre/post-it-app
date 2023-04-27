7fb9e9a75c00dae1e445b81cf00f1a9406c946ff
eobcre <eobcre@gmail.com>
2023-04-27 15:23:02 -0400

    grid layout fixed

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -2,7 +2,7 @@ import Note from './Note';
 
 const NoteList = () => {
   return (
-    <div className='note-layout grid gap-4 justify-items-center p-5'>
+    <div className='note-layout grid grid-cols-custom gap-4 justify-items-center p-5'>
       <Note />
       <Note />
       <Note />
