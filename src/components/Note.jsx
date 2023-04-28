3213d01ee1bc0b9cde9e81a96b676e2b6d418ecb
eobcre <eobcre@gmail.com>
2023-04-27 23:05:54 -0400

    props received from NoteData and implemented

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,11 @@
 import { RiDeleteBin2Line } from 'react-icons/Ri';
 
-const Note = () => {
+const Note = ({ id, text, date }) => {
   return (
     <div className='flex flex-col justify-between bg-neon-yellow rounded-md w-72 h-72 p-5'>
-      <h1>Title</h1>
+      <h1>{text}</h1>
       <footer className='flex justify-between items-center'>
-        <small>Date</small>
+        <small>{date}</small>
         <RiDeleteBin2Line className='text-2xl' />
       </footer>
     </div>
