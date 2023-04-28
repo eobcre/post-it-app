bba11308a2970991003b95ac8ddfc3cc185002a1
eobcre <eobcre@gmail.com>
2023-04-27 22:47:19 -0400

    note details data

---
+++ NoteData.jsx
@@ -0,0 +1,19 @@
+import { nanoid } from 'nanoid';
+
+export const NoteData = [
+  {
+    id: nanoid(),
+    text: 'Note',
+    date: '4/27/2023',
+  },
+  {
+    id: nanoid(),
+    text: 'Note',
+    date: '4/27/2023',
+  },
+  {
+    id: nanoid(),
+    text: 'Note',
+    date: '4/27/2023',
+  },
+];
