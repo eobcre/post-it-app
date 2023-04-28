3cb82cee912b4294a57767683fd44617c760fe39
eobcre <eobcre@gmail.com>
2023-04-28 00:00:04 -0400

    height fixed

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,7 @@ import { RiDeleteBin2Line } from 'react-icons/Ri';
 
 const Note = ({ id, text, date }) => {
   return (
-    <div className='flex flex-col justify-between bg-neon-yellow rounded-md w-72 h-72 p-5'>
+    <div className='flex flex-col justify-between bg-neon-yellow rounded-md w-72 h-64 p-5'>
       <h1>{text}</h1>
       <footer className='flex justify-between items-center'>
         <small>{date}</small>
