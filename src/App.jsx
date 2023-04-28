8b74f44013d88f8bad57eef5fc782f23730bcca2
eobcre <eobcre@gmail.com>
2023-04-27 22:53:52 -0400

    useState settings and props passed to NoteList

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+// Hooks
+import { useState } from 'react';
+// Component
 import NoteList from './components/NoteList';
+// Data
+import { NoteData } from '../NoteData';
 
 const App = () => {
+  const [notes, setNotes] = useState(NoteData);
+
   return (
-    <div>
-      <NoteList />
+    <div className='max-w-4xl mx-auto'>
+      <NoteList notes={notes} />
     </div>
   );
 };
