ead44e34c7929465055076ec8e2628ca9f1802a3
eobcre <eobcre@gmail.com>
2023-04-27 23:54:34 -0400

    note layout and styled

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,5 +1,20 @@
+// react-icons
+import { AiOutlineSave } from 'react-icons/Ai';
+
 const AddNote = () => {
-  return <div></div>;
+  return (
+    <div className='flex flex-col justify-between bg-neon-yellow rounded-md w-72 p-5'>
+      <textarea
+        cols='10'
+        rows='8'
+        placeholder='Type your note.'
+        className='bg-neon-yellow outline-none resize-none'
+      ></textarea>
+      <footer className='flex justify-end'>
+        <AiOutlineSave className='text-2xl' />
+      </footer>
+    </div>
+  );
 };
 
 export default AddNote;
