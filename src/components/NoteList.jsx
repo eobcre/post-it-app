ee83f305615533129a3b3b284222b05ec82dd5bd
eobcre <eobcre@gmail.com>
2023-04-29 21:54:27 -0400

    map loop fixed, key added

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -5,8 +5,8 @@ import AddNote from './AddNote';
 const NoteList = ({ notes }) => {
   return (
     <div className='grid grid-cols-custom gap-6 p-5'>
-      {notes.map((note) => (
-        <Note id={note.id} text={note.text} date={note.date} />
+      {notes.map(({ id, text, date }) => (
+        <Note key={id} id={id} text={text} date={date} />
       ))}
       <AddNote />
     </div>
