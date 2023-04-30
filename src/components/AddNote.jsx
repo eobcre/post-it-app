c1cc49e68b97a6bce97ddb4049fccb878ac43843
eobcre <eobcre@gmail.com>
2023-04-29 22:20:17 -0400

    useState settings, handleChange logic

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 // react-icons
 import { AiOutlineSave } from 'react-icons/Ai';
 
 const AddNote = () => {
+  const [noteText, setNoteText] = useState('');
+
+  const handleChange = (e) => {
+    setNoteText(e.target.value);
+  };
+
   return (
     <div className='flex flex-col justify-between bg-neon-yellow rounded-md w-72 h-64 p-5'>
       <textarea
@@ -9,6 +16,8 @@ const AddNote = () => {
         rows='8'
         placeholder='Type your note.'
         className='bg-neon-yellow outline-none resize-none'
+        value={noteText}
+        onChange={handleChange}
       ></textarea>
       <footer className='flex justify-end'>
         <AiOutlineSave className='text-2xl hover:opacity-50 cursor-pointer' />
