e908e1fb2cb07caa3e39a7b06b8c28aa3871a5ba
eobcre <eobcre@gmail.com>
2023-05-05 12:17:35 -0400

    passed handleDeleteNote as props, created logic in delete icon

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,15 @@
 import { RiDeleteBin2Line } from 'react-icons/Ri';
 
-const Note = ({ id, text, date }) => {
+const Note = ({ id, text, date, handleDeleteNote }) => {
   return (
     <div className='flex flex-col justify-between bg-post-yellow rounded-md w-72 h-64 p-5'>
       <h1>{text}</h1>
       <footer className='flex justify-between items-center'>
         <small>{date}</small>
-        <RiDeleteBin2Line className='text-2xl hover:opacity-50 cursor-pointer' />
+        <RiDeleteBin2Line
+          onClick={() => handleDeleteNote(id)}
+          className='text-2xl hover:opacity-50 cursor-pointer'
+        />
       </footer>
     </div>
   );
