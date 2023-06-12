8fac51b6ec5a291ca5fa7889212a560f70f053bc
eobcre <eobcre@gmail.com>
2023-06-12 15:34:04 -0400

    new component created

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+// img
+import Logo from '../assets/logo.png';
+
+const Header = () => {
+  return (
+    <div className='p-4'>
+      <img src={Logo} alt='Logo' width='200' />
+    </div>
+  );
+};
+
+export default Header;
