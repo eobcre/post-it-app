786e1b63f59fde8db95f02340e2ba39bc696ff34
eobcre <eobcre@gmail.com>
2023-06-15 13:24:13 -0400

    footer position and styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <>
+    <div className='flex justify-center items-center h-24'>
       <small>&copy; 2023 Emmy Manning</small>
-    </>
+    </div>
   );
 };
 
